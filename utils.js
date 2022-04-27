cae5fa2f5ab973e30a22a8c942c4018e026d9483
Léa Wawro <leawawro001@gmail.com>
2022-04-27 09:48:35 +0200

    template improvement

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ function rnd(pMin,pMax){
     return (Math.random()*(pMax-pMin))+pMin
 }
 
-function getAngle(pX1,pY1,pX2,pY2){
+function angle(pX1,pY1,pX2,pY2){
     return Math.atan2((pY2-pY1),(pX2-pX1));
 }
 
@@ -27,4 +27,14 @@ function drawCircle(pCtx,pX,pY,pRadius,pColorFill,pColorStroke){
     pCtx.arc(pX,pY,pRadius,0,2*Math.PI);
     pCtx.fill();
     pCtx.stroke();
+}
+
+function drawTriangle(pCtx,pX,pY,pX2,pY2,pX3,pY3,pColor){
+    
+    pCtx.beginPath();
+    pCtx.fillStyle="rgba("+pColor+")";
+    pCtx.moveTo(pX,pY);
+    pCtx.lineTo(pX2,pY2);
+    pCtx.lineTo(pX3,pY3);
+    pCtx.fill();
 }
